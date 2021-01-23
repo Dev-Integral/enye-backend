5bcfbf8024468aa12c09fcd2a0d64e93561e200d
Ayoola <tayoola13@yahoo.com>
2021-01-23 17:28:25 +0100

    api/rates?base&currency...: Now works great

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const https = require('https')
 const port = 3000;
 const bodyParser = require('body-parser');
+const { query } = require('express');
 
 let exchangeRate = '';
 
@@ -21,7 +22,6 @@ let request = https.get('https://api.exchangeratesapi.io/latest', (res) => {
         res.on('close', () => {
             console.log('Retrieved all data');
             exchangeRate = (JSON.parse(data));
-            console.log(exchangeRate);
         });
         request.on('error', (err) => {
             console.error(`Encountered an error trying to make request:${err.message}`)
@@ -33,20 +33,48 @@ let request = https.get('https://api.exchangeratesapi.io/latest', (res) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/api/rates', (req, res) => {
-    if (req.query.base) {
-        let rates = exchangeRate.rates
+    if (req.query.base && req.query.currency) {
+        let rates = exchangeRate.rates;
+        let updatedData = {};
+            updatedData['date'] = exchangeRate.date;
+            
+        let currency = req.query.currency.split(',');
 
-        const allowed = ['EUR', 'CZK','KRW']
+        let request = https.get(`https://api.exchangeratesapi.io/latest${req.query.base}`, (res) => {
+            if (res.statusCode !== 200) {
+                console.error(`Couldn't get data`);
+                res.resume();
+                return;
+            } else {
+                let data = '';
+
+                res.on('data', (chunk) => {
+                    data += chunk;
+                });
+                res.on('close', () => {
+                    console.log('Retrieved all data');
+                    exchangeRate = (JSON.parse(data));
+                    console.log(exchangeRate);
+                });
+                request.on('error', (err) => {
+                    console.error(`Encountered an error trying to make request:${err.message}`)
+                })
+            }
+        });
+
         const results = Object.keys(rates)
-        .filter(key => allowed.includes(key))
-        .reduce((obj, key) => {
-            obj[key]= rates[key];
-            return obj;
-        }, {});
+            .filter(key => currency.includes(key))
+            .reduce((obj, key) => {
+                obj[key] = rates[key];
+                return obj;
+            }, {});
         
-        console.log(results);
+        updatedData['base'] = req.query.base;
+        updatedData['rates'] = results
 
-        return res.json({ 'results': results });
+        console.log(results, currency);
+
+        return res.json({ 'results': updatedData });
     } else {
         return res.json({ 'results': exchangeRate });
     }
